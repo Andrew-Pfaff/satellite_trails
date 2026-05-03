c748ef99398410d9db2a7870635bf62be643da22
Andrew-Pfaff <andrew.pfaff2@gmail.com>
2026-05-03 21:08:15 +0100

    Added loss functions, fixed model size

--- src/satellite_trail_segmentation/model/losses.py
+++ src/satellite_trail_segmentation/model/losses.py
@@ -1,9 +1,17 @@
-def bce_loss(image, mask):
+import torch
+
+_bce = torch.nn.BCEWithLogitsLoss() #Loss function that applies sigmoid and calculates BCE
+
+def bce_loss(pred, mask):
+    loss = _bce(pred,mask)
     return loss
 
-def dice_loss(image, mask):
+def dice_loss(pred, mask):
+    epsilon = 10e-5
+    prob = torch.sigmoid(pred)
+    loss = 1 - (2*torch.sum(prob*mask) + epsilon) / (torch.sum(prob) + torch.sum(mask) + epsilon)
     return loss
 
-def combo_loss(image, mask, bce_weight=0.5, dice_weight=0.5):
-    loss = bce_weight*bce_loss(image,mask) + dice_weight*dice_loss(image,mask)
+def combo_loss(pred, mask, bce_weight=0.5, dice_weight=0.5):
+    loss = bce_weight*bce_loss(pred,mask) + dice_weight*dice_loss(pred,mask)
     return loss
