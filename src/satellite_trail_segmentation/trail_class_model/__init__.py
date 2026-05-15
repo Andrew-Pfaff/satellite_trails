e62187d7fbc777e2a3618ce1548595cf882888d7
Andrew-Pfaff <110065226+Andrew-Pfaff@users.noreply.github.com>
2026-05-15 11:10:11 +0100

    Cleanup and benchmarking num workers and batch size (#18)
    * Cleanup and added benchmark script
    
    * Benchmarking slurm script update
    
    ---------
    
    Co-authored-by: Andrew Pfaff <anp50@cam.ac.uk>

--- src/satellite_trail_segmentation/trail_class_model/__init__.py
+++ src/satellite_trail_segmentation/trail_class_model/__init__.py
@@ -1,5 +1,5 @@
 from satellite_trail_segmentation.trail_class_model.classifier import TrailClassifier
-from satellite_trail_segmentation.trail_class_model.evaluate import recreate_full_field
+from satellite_trail_segmentation.trail_class_model.evaluate import predict, recreate_full_field
 from satellite_trail_segmentation.trail_class_model.losses import recall_combo_loss, weighted_bce_loss
 from satellite_trail_segmentation.trail_class_model.metrics import batch_metrics
 from satellite_trail_segmentation.trail_class_model.train import train_classifier
@@ -8,6 +8,7 @@ __all__ = [
     "TrailClassifier",
     "recreate_full_field",
     "batch_metrics",
+    "predict",
     "recall_combo_loss",
     "train_classifier",
     "weighted_bce_loss",
