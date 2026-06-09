198f2d172ad49e7c113c7b64dbb598cb0e740623
Andrew Pfaff <anp50@cam.ac.uk>
2026-06-09 18:52:44 +0100

    Removed batch metrics

--- scripts/python/evaluate_models.py
+++ scripts/python/evaluate_models.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 from satellite_trail_segmentation.evaluation.unet_evaluate import evaluate_dataset_unet
 from satellite_trail_segmentation.evaluation.classifier_evaluate import evaluate_dataset_classifier
@@ -16,7 +15,7 @@ if __name__ == "__main__":
     unet_batch = 64
     classifier_batch = 128 
 
-    unet_model_path = "/home/anp50/rds/hpc-work/satellite_trails/results/models/unet2/unet_weights.pt"
+    unet_model_path = "/home/anp50/rds/hpc-work/satellite_trails/results/models/unet/unet_weights.pt"
     classifier_model_path = "/home/anp50/rds/hpc-work/satellite_trails/results/models/classifier/classifier_weights.pt"
 
     unet_model = UNet()
@@ -29,7 +28,7 @@ if __name__ == "__main__":
 
     test_thresholds = list(np.linspace(0.05, 0.95, 19))
 
-    unet_metrics_counts, unet_patch_metrics, fpr, tpr, thresholds, optimal_threshold, roc_auc = evaluate_dataset_unet(unet_model, h5_path, split, test_thresholds, unet_batch)
+    unet_metrics_counts, fpr, tpr, thresholds, optimal_threshold, roc_auc = evaluate_dataset_unet(unet_model, h5_path, split, test_thresholds, unet_batch)
 
     print("Unet metrics: ")
     print(unet_metrics_counts)
@@ -42,8 +41,7 @@ if __name__ == "__main__":
     print(classifier_image_wise_counts)
 
 
-    plot_roc_curve(fpr, tpr, thresholds, roc_auc, optimal_threshold, save_path="/home/anp50/rds/hpc-work/satellite_trails/results/models/unet2/unet_val_roc_metrics.png")
-    plot_threshold_metrics(unet_metrics_counts, save_path="/home/anp50/rds/hpc-work/satellite_trails/results/models/unet2/unet_val_metrics.png")
-    plot_threshold_metrics(unet_patch_metrics, save_path="/home/anp50/rds/hpc-work/satellite_trails/results/models/unet2/unet_val_metrics_patch.png")
+    plot_roc_curve(fpr, tpr, thresholds, roc_auc, optimal_threshold, save_path="/home/anp50/rds/hpc-work/satellite_trails/results/models/unet/unet_val_roc_metrics.png")
+    plot_threshold_metrics(unet_metrics_counts, save_path="/home/anp50/rds/hpc-work/satellite_trails/results/models/unet/unet_val_metrics.png")
     plot_threshold_metrics(classifier_metrics, save_path="/home/anp50/rds/hpc-work/satellite_trails/results/models/classifier/classifier_val_metrics.png")
     
